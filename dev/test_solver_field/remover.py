f50d739dad8b029b07d8ebd66120812bfeaf41b7
Mohamed TRAORE <39139566+apollosoldier@users.noreply.github.com>
2022-11-09 11:31:23 +0100

    Update remover.py

--- dev/test_solver_field/remover.py
+++ dev/test_solver_field/remover.py
@@ -4,36 +4,61 @@ import skimage.exposure
 from PIL import Image
 from PIL import ImageDraw
 
-def generate_green_mask(image_path:str) -> object:
-    img = cv2.imread(image_path)
-    lab = cv2.cvtColor(img,cv2.COLOR_BGR2LAB)
-    A = lab[:,:,1]
-    thresh = cv2.threshold(A, 3, 255, 
-                           cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+def remove_green_stars(image_path:str) -> tuple[object, 
+                                                object, 
+                                                object, 
+                                                object]:
+    blur  : object 
+    mask  : object
+    img   : object 
+    lab   : object
+    A     : object
+    thresh: object
+    result: object
     
-    blur = cv2.GaussianBlur(thresh, (0,0), sigmaX=5, sigmaY=5, borderType = cv2.BORDER_DEFAULT)
+    try:
+        img = cv2.imread(image_path)
     
-    mask = skimage.exposure.rescale_intensity(blur, 
-                                              in_range=(0,255), 
-                                              out_range=(0,255)).astype(np.uint8)
-    result = img.copy()
-    result = cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
-    result[:,:,3] = mask
-    mask_red_spots = skimage.exposure.rescale_intensity(thresh, 
+        lab = cv2.cvtColor(img,cv2.COLOR_BGR2LAB)
+
+        A = lab[:,:,1]
+
+        thresh = cv2.threshold(A, 3, 255, 
+                               cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+        
+        blur = cv2.GaussianBlur(thresh, (0,0), 
+                                sigmaX=5, 
+                                sigmaY=5, 
+                                borderType = cv2.BORDER_DEFAULT)
+    except BaseException as be:
+        raise be.__class__
+        
+    if blur is not None:
+        mask = skimage.exposure.rescale_intensity(blur, 
+                                                  in_range=(0,255), 
+                                                  out_range=(0,255)).astype(np.uint8)
+        result = img.copy()
+        result = cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
+        result[:,:,3] = mask
+        mask_red_spots = skimage.exposure.rescale_intensity(thresh, 
                                                         in_range=(0,255), 
                                                         out_range=(0,255)).astype(np.uint8)
     
-    a_channel = np.ones(mask_red_spots.shape, dtype=np.float)/2.0
-    mask_red_spots_image = mask_red_spots*a_channel
-    mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    
-    cv2.imwrite('make_greenscreen_thresh.png', thresh)
-    cv2.imwrite('make_greenscreen_mask.png', mask)
-    cv2.imwrite('make_greenscreen_antialiased.png', result)
-    cv2.imwrite('make_greenscreen_blur.png', blur)
-    cv2.imwrite('make_mask_red_spots.png', mask_red_spots)
+        a_channel = np.ones(mask_red_spots.shape, dtype=np.float)/2.0
+        mask_red_spots_image = mask_red_spots*a_channel
+    else:
+        raise BaseException.__class__
+        
     return thresh, mask, result, blur, mask_red_spots
+
 
 if __name__ == "__main__":
     
-    generate_green_mask("7295761_red_green.png")
+    thresh, mask, result, blur, mask_red_spots = remove_green_stars("7295761_red_green.png")
+    if thresh is not None and mask is not None and result is not None and blur is not None and mask_red_spots is not None:
+        cv2.imwrite('make_greenscreen_thresh.png', thresh)
+        cv2.imwrite('make_greenscreen_mask.png', mask)
+        cv2.imwrite('make_greenscreen_antialiased.png', result)
+        cv2.imwrite('make_greenscreen_blur.png', blur)
+        cv2.imwrite('make_mask_red_spots.png', mask_red_spots)
+
