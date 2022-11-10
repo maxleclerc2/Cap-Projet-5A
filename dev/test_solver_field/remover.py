f907a38e07eb50312f37614a1ec0fb7a5a2e569d
Mohamed TRAORE <39139566+apollosoldier@users.noreply.github.com>
2022-11-10 15:14:45 +0100

    Update remover.py

--- dev/test_solver_field/remover.py
+++ dev/test_solver_field/remover.py
@@ -60,15 +60,49 @@ def remove_green_stars(image_path:str) -> tuple[object,
         raise BaseException.__class__
         
     return thresh, mask, result, blur, mask_red_spots
+  
+  
+def green_spot_cleaner(base_image_path:str, mask_red_spots:object) -> object:
+
+    background_image = cv2.imread(base_image_path)
+    background_image = cv2.cvtColor(background_image, cv2.COLOR_BGR2RGB)
+    crop_background = background_image.copy()
+
+    crop_background[mask_red_spots != 0] = [0,0,0]
+
+    kernel3 = np.array([[0,  -1,  0],
+                        [-1,  5, -1],
+                        [0, -1,  0]]) 
+
+    plt.imshow(crop_background)
+    cv2.imwrite('crop_background.png', crop_background)
+    plt.show()
+    return crop_background
 
 
 if __name__ == "__main__":
     
     thresh, mask, result, blur, mask_red_spots = remove_green_stars("7295761_red_green.png")
+
     if thresh is not None and mask is not None and result is not None and blur is not None and mask_red_spots is not None:
         cv2.imwrite('make_greenscreen_thresh.png', thresh)
         cv2.imwrite('make_greenscreen_mask.png', mask)
         cv2.imwrite('make_greenscreen_antialiased.png', result)
         cv2.imwrite('make_greenscreen_blur.png', blur)
         cv2.imwrite('make_mask_red_spots.png', mask_red_spots)
+
+    try:
+
+        crop_background = green_spot_cleaner('kaEC6_-_modified.jpg', thresh)
+
+        background_image = cv2.imread('kaEC6_-_modified.jpg')
+        background_image = cv2.cvtColor(background_image, cv2.COLOR_BGR2RGB)
+        crop_background = background_image.copy()
+
+        crop_background[mask_red_spots != 0] = [0,0,0]
+        #crop_background[mask_red_spots == 0] = [255,255,255]
+
+        cv2.imwrite('crop_background.png', crop_background)
+    except BaseException as be:
+        raise be.__class__
 
